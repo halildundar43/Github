66961cce1c7124643682f2b74055d9f53e3c7036
halildundar43 <81415608+halildundar43@users.noreply.github.com>
2021-04-07 16:13:59 +0300

    çakıştı

--- src/gitTest/TestClass.java
+++ src/gitTest/TestClass.java
@@ -7,6 +7,9 @@ public class TestClass {
 		System.out.println("Git ten merhaba"); // github taki deðiþiklikten geldi
 		System.out.println("version");
 		System.out.println("version2");	
-		System.out.println("version4");
+		System.out.println("vv");
+		System.out.println("version3");
+		
+		System.out.println("vv");
 	}
 }
