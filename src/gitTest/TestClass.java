0c530cbdca4896fbfedb2a2e45346a9af2cbaf84
halildundar43 <81415608+halildundar43@users.noreply.github.com>
2021-04-07 16:17:52 +0300

    Update TestClass.java

--- src/gitTest/TestClass.java
+++ src/gitTest/TestClass.java
@@ -10,7 +10,7 @@ public class TestClass {
 		System.out.println("vv");
 		System.out.println("version3");
 		
-		System.out.println("version4");
+		System.out.println("ver");
 		System.out.println("vv");
 	}
 }
