1934c086dc831a207462d397be9d5a00e999ebb5
halildundar43 <81415608+halildundar43@users.noreply.github.com>
2021-04-07 15:53:52 +0300

    Update TestClass.java

--- src/gitTest/TestClass.java
+++ src/gitTest/TestClass.java
@@ -4,8 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git ten merhaba");
-		System.out.println("Git ten merhaba"); // github taki değişiklikten geldi
+		System.out.println("Git ten merhaba"); // github taki deÃ°iÃ¾iklikten geldi
 		System.out.println("version");
-		
+		System.out.println("version2");		
 	}
 }
